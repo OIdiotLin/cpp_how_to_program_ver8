d535e1483f87233f50d28c46ee59f38f9129b4ef
OIdiot <lhd1997@qq.com>
2016-11-04 21:35:11 +0800

    finished

--- chapter5/5-18.cpp
+++ chapter5/5-18.cpp
@@ -2,13 +2,15 @@
 // System:	Win 10
 // Problem:	5.18
 #include <iostream>
+#include <bitset>
 #include <iomanip>
 using namespace std;
 
 int main() {
-	cout << setw(3) << "DEC" << setw(5) << "OCT" << setw(5) << "HEX" << endl;
+	cout << setw(3) << "DEC" <<setw(10)<<"BIN"<< setw(5) << "OCT" << setw(5) << "HEX" << endl;
 	for (int i = 1;i <= 256;i++) {
 		cout << setw(3) << dec << i;
+		cout << setw(10) << bitset<sizeof(uint8_t)*9>(i);
 		cout << setw(5) << oct << i;
 		cout << setw(5) << hex << i << endl;
 	}
