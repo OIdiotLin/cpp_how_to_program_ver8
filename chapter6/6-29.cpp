e7cf2c1099fd6a94ca781baaf9b2cee73510ae81
OIdiot <lhd1997@qq.com>
2016-11-16 18:41:37 +0800

    Prime Numbers

--- chapter6/6-29.cpp
+++ chapter6/6-29.cpp
@@ -3,7 +3,9 @@
 // Problem:	6.29 (Prime Numbers)
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
+#define DEBUG
 
 // Using Miller-Rabbin Prime Testing
 // calculate base^exponent%mod
@@ -15,6 +17,7 @@ long long expMod(long long base, long long exponent, long long mod) {
 			res %= mod;
 		}
 		base *= base;
+		base %= mod;
 		exponent >>= 1;
 	}
 	return res;
@@ -28,7 +31,21 @@ bool fermatTest(long long p) {
 	return expMod(a, p - 1, p) == 1;
 }
 // Miller-Rabbin Prime Testing
+#define TEST_COUNTER 80	// Pick test_counter number as base for test
+bool isPrime(long long number) {
+	if (number == 0 || number == 1)
+		return false;
+	for (int i = 0;i < TEST_COUNTER;i++)
+		if (!fermatTest(number))
+			return false;
+	return true;
+}
 
+#define UPPER_LIM 10000
 int main() {
-
+	cout << "The prime numbers in [1,10000] are :" << endl;
+	for (int i = 3;i <= UPPER_LIM;i++)
+		if (isPrime(i))
+			cout << setw(5) << i << endl;
+	return 0;
 }
