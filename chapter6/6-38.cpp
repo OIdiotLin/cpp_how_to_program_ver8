bae89b79001e398ff159d81a86247b47b2fcc76d
OIdiot <lhd1997@qq.com>
2016-11-16 20:58:01 +0800

    Towers of Hanoi

---
+++ chapter6/6-38.cpp
@@ -0,0 +1,31 @@
+// Author:	LHD
+// System:	Win 10
+// Problem:	6.33 (Towers of Hanoi)
+#include <iostream>
+using namespace std;
+
+class HanoiTower {
+public:
+	HanoiTower() {}
+	~HanoiTower() {}
+	void move(int currentDisk, int start, int temp, int end) {
+		if (currentDisk == 1) {
+			cout << start << " --> " << end << endl;
+			return;
+		}
+		move(currentDisk - 1, start, end, temp);
+		cout << start << " --> " << end << endl;
+		move(currentDisk - 1, temp, start, end);
+	}
+};
+
+int main() {
+	int n;
+	HanoiTower tower;
+	while (true) {
+		cout << "Enter the amount of disks: ";
+		cin >> n;
+		tower.move(n, 1, 2, 3);
+	}
+	return 0;
+}
