14142791a73e6a66ea1f31632dce6b5840de7357
OIdiot <lhd1997@qq.com>
2016-11-16 12:30:27 +0800

    Separating Digits

--- chapter6/6-24.cpp
+++ chapter6/6-24.cpp
@@ -4,6 +4,39 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+// Part a)
+int quotient(const int& dividend, const int& divisor) {
+	return dividend / divisor;
+}
 
+//Part b)
+int remainder(const int& dividend, const int& divisor) {
+	return dividend%divisor;
+}
+
+// Part c)
+void separate(int number) {
+	int divisor = 10000;
+	while (number < divisor)
+		divisor = quotient(divisor, 10);
+	while (divisor) {
+		cout << quotient(number, divisor) << " ";
+		number = remainder(number, divisor);
+		divisor = quotient(divisor, 10);
+	}
+	cout << endl << endl;
+}
+
+int main() {
+	int number;
+	while (true) {
+		cout << "Enter a number (1~32767): ";
+		cin >> number;
+		if (number < 1 || number >32767) {
+			cout << "OVERFLOW!!" << endl;
+			continue;
+		}
+		separate(number);
+	}
+	return 0;
 }
