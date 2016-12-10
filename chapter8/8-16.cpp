2580534662c006c52152cfac9108c1aceafbbf8b
OIdiot <lhd1997@qq.com>
2016-12-10 20:51:19 +0800

    Maze Traversal

--- chapter8/8-16.cpp
+++ chapter8/8-16.cpp
@@ -10,6 +10,8 @@ using std::cout;
 using std::cin;
 using std::endl;
 
+#define SHOW_INTERVAL 150
+
 inline void delay(int interval) {
 	clock_t t = clock();
 	while (clock() - t < interval);
@@ -22,6 +24,10 @@ class Maze {
 #define WALL '#'
 #define MAX_BOARD 12
 private:
+
+	int size;
+	char map[MAX_BOARD][MAX_BOARD];
+
 	struct Position {
 		int x, y;
 		bool operator == (const Position &t)const {
@@ -49,18 +55,64 @@ private:
 		void goNext() {
 			cur = next();
 		}
-		bool hasWallOnRightside() {
-
+		bool hasWallOnRightside(char map[MAX_BOARD][MAX_BOARD]) {
+			Explorer tmp = *(this);
+			tmp.turnRight();
+			Position R = tmp.next();
+			return map[R.x][R.y] == WALL;
+		}
+		void turnRight() {
+			switch (towards) {
+			case up:
+				towards=right;
+				break;
+			case down:
+				towards = left;
+				break;
+			case left:
+				towards = up;
+				break;
+			case right:
+				towards = down;
+				break;
+			}
+		}
+		void turnLeft() {
+			switch (towards) {
+			case up:
+				towards = left;
+				break;
+			case down:
+				towards = right;
+				break;
+			case left:
+				towards = down;
+				break;
+			case right:
+				towards = up;
+				break;
+			}
 		}
 	};
 
-	int size;
-	char map[MAX_BOARD][MAX_BOARD];
 	Position entry, exit;
 	Explorer explr;
 
 	bool isValid(const Position &t)const {
 		return t.x >= 0 && t.x < size && t.y >= 0 && t.y < size && map[t.x][t.y] != WALL;
+	}
+	void show() {
+		system("cls");
+		for (int i = 0;i < size;i++) {
+			for (int j = 0;j < size;j++) {
+				if (explr.cur == Position{ i,j })
+					cout << '@';
+				else
+					cout << map[i][j];
+			}
+			cout << endl;
+		}
+		delay(SHOW_INTERVAL);
 	}
 public:
 	Maze() {}
@@ -89,38 +141,37 @@ public:
 		}
 		entry = a[0], exit = a[1];
 	}
-	void show() {
-		system("cls");
-		for (int i = 0;i < size;i++) {
-			for (int j = 0;j < size;j++) {
-				if (explr.cur == Position{ i,j })
-					cout << '@';
-				else
-					cout << map[i][j];
-			}
-			cout << endl;
-		}
-	}
 	void start() {
 		if (entry.x == 0)	explr.towards = down;		// towards initialization
 		else if (entry.x == size - 1)	explr.towards = up;
 		else if (entry.y == 0)	explr.towards = right;
 		else if (entry.y == size - 1)	explr.towards = left;
-		explr.goNext();	// enter the entry
+		explr.cur = entry;	// enter the entry
 
 		while (explr.cur != exit) {		// loop until get the exit
-			if(explr.hasWallOnRightside())
+			if (!explr.hasWallOnRightside(map))
+				explr.turnRight();
 
-			if (isValid(explr.next()))	// if next position is on the path
+			if (isValid(explr.next())) {	// if next position is on the path
 				explr.goNext();		// go on
-
-
+				show();
+			}
+			else {
+				explr.turnLeft();
+			}
 		}
-
 
 	}
 };
 
+Maze G;
 int main() {
+	cout << "The map data are stored in /maze.txt " << endl;
+	system("pause");
+	freopen("maze.txt", "r", stdin);
 	std::ios::sync_with_stdio(false);
+	G.readSize();
+	G.readMap();
+	G.findEntryAndExit();
+	G.start();
 }
